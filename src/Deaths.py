68759a3c67e9509a211392966939f09a7e8e7c5d
peroza <perrozz95@gmail.com>
2021-12-04 18:00:50 +0100

    Deaths class introduced

---
+++ src/Deaths.py
@@ -0,0 +1,25 @@
+"""
+File name: Keyword.py
+Author: HOJA
+Date created: 1/11/2021
+Date last modified: 12/11/2021
+Python Version: 3.8
+"""
+
+
+import pandas as pd
+
+
+class Deaths:
+
+    def __init__(self, name, columns, static_columns):
+        self.name = name
+        self.deaths = pd.DataFrame(
+            columns=columns)
+        self.deaths_percentage = ""
+        self.death_columns = columns.remove(static_columns)
+
+    def plot_all_death_cols(self):
+        for col in self.death_columns:
+            continue
+            
