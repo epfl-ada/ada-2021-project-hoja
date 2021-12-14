f6109ddafc3cd7315a63630940bc725a96921670
peroza <perrozz95@gmail.com>
2021-12-14 15:05:12 +0100

    Refactored constants file

--- src/CONSTS.py
+++ src/CONSTS.py
@@ -6,6 +6,7 @@ Date last modified: 12/11/2021
 Python Version: 3.8
 """
 
+# File Paths
 DATA_PATH = './data/'
 GENERATED_PATH = './generated/'
 KEYWORDS_FILE_PATH = DATA_PATH + "keywords.txt"
@@ -15,15 +16,23 @@ SPEAKER_ATTRIBUTES_PATH = DATA_PATH + "speaker_attributes.parquet"
 URL_END_PATH = DATA_PATH + "country_url_end.txt"
 URL_COUNTRY_PATH = DATA_PATH + "url_country_lib.json"
 
+# Colors
 COLORS = ["red", "green", "blue", "brown", "yellow", "purple", "orange", "pink"]
 
-TOPICS_FOR_CLUSTERING =  ["fire, heat, and hot substances",
+# Topics for clustering
+TOPICS_FOR_CLUSTERING = ["fire, heat, and hot substances",
                           "environmental heat and cold exposure",
                           "road injuries",
                           "war and terrorism",
                           "interpersonal violence",
                           "poisonings",
                           "drowning"]
+
+# Keyword Stopword
+KEYWORD_STOPWORDS = ['consumption',
+                     'piles',
+                     'intoxication']
+
 # To add country of speaker
 COUNTRY_IDENTIFIER = dict()
 SPEAKER_ATTRIBUTES = dict()
@@ -32,8 +41,9 @@ SPEAKER_ATTRIBUTES = dict()
 URL_END_LIB = dict()
 URL_COUNTRY = dict()
 
-BEGIN_YEAR = 2011
-END_YEAR = 2011
+# Years for data processing
+BEGIN_YEAR = 2008
+END_YEAR = 2017
 
 # Deaths object constants
 DEATHS_INFO_COLUMNS = ["Entity", "Code", "Year"]
